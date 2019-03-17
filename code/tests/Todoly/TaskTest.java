87136a5cabcaeaf0ec29594cb830bbdfea7d9ccc
SonakshiGuru <chiki.sona@gmail.com>
2019-03-17 16:15:38 +0100

    Updated

--- code/tests/Todoly/TaskTest.java
+++ code/tests/Todoly/TaskTest.java
@@ -2,24 +2,58 @@ package Todoly;
 
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 //import static org.junit.Assert.assertEquals;
 
 
 public class TaskTest {
     private Task t;
     private String name = "TaskName X";
-    private Date date;
-    private DATE
+    private String date = "10-10-2019";
+    //private SimpleDateFormat formatter = new SimpleDateFormat("DD-MM-YYYY");
+    //private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("DD-MM-YYYY");
+    // private LocalDate dt = LocalDate.parse(date, dtf);
+    // private Date dueDate = formatter.parse(dt.format(dtf));
+
     private String status = "Complete";
     private String project = "Mission Impossible";
+
+/*
+    public TaskTest(){
+        t = new Task(name, Util.parseDate(date), project);
+    }*/
 
 
     @Test
     public void GetTaskName() {
         try {
-            assertEquals(name, )
+            assertEquals(project, t.getName());
+            System.out.println("Test Successful !");
+        } catch (Exception e) {
+            System.out.println("Test Failed !");
         }
+
+    }
+
+    @Test
+    public void GetTaskName2() {
+        try {
+            assertNotEquals(date, t.getName());
+            System.out.println("Test Successful !");
+        } catch (Exception e) {
+            System.out.println("Test Failed !");
+        }
+    }
+
+
 
         /*try {
             String taskName = "hello";
@@ -39,6 +73,4 @@ public class TaskTest {
 
         }*/
 
-
-    }
 }
---
+++ code/tests/Todoly/UtilTest.java
@@ -0,0 +1,16 @@
+package Todoly;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    @Test
+    void parseDate() {
+    }
+
+    @Test
+    void parseDate1() {
+    }
+}
